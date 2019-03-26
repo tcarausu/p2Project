58cdf9234d9df0fc969400f0a711bd7f31ccf4c6
tcarausu <tcarausu@gmail.com>
2019-03-26 20:16:58 +0100

    0.0.1

---
+++ app/src/main/java/com/example/myapplication/utility_classes/UniversalImageLoader.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.utility_classes;
+
+import android.content.Context;
+
+import com.example.myapplication.R;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+/**
+ * File created by tcarau18
+ **/
+public class UniversalImageLoader {
+
+    private static final int defaultImage = R.drawable.ic_launcher_foreground;
+
+    private Context mContext;
+
+    public UniversalImageLoader(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public ImageLoaderConfiguration getConfig() {
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .showImageOnLoading(defaultImage)
+                .showImageForEmptyUri(defaultImage)
+                .showImageOnFail(defaultImage)
+                .cacheOnDisk(true).cacheInMemory(true)
+                .cacheOnDisk(true).resetViewBeforeLoading(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        return new ImageLoaderConfiguration.Builder(mContext)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 * 1024).build();
+
+    }
+}
