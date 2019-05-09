4354c2565c4afd06a86b99d76983b1b1128572af
Dimos <dimos_samos@hotmail.com>
2019-05-09 12:32:16 +0200

    History Log

--- app/src/main/java/com/example/myapplication/utility_classes/BottomNavigationViewHelper.java
+++ app/src/main/java/com/example/myapplication/utility_classes/BottomNavigationViewHelper.java
@@ -6,7 +6,7 @@ import android.util.Log;
 
 import com.example.myapplication.R;
 import com.example.myapplication.home.HomeActivity;
-import com.example.myapplication.likes.LikePostActivity;
+import com.example.myapplication.history_log.HistoryLogActivity;
 import com.example.myapplication.post.AddPostActivity;
 import com.example.myapplication.search.SearchActivity;
 import com.example.myapplication.user_profile.UserProfileActivity;
@@ -46,23 +46,24 @@ public class BottomNavigationViewHelper {
                             .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
                     break;
 
-                case R.id.ic_add_post:
-                    Intent addPostIntent = new Intent(context, AddPostActivity.class);//ACTIVITY_NUM = 2
-                    context.startActivity(addPostIntent
-                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                    break;
-                case R.id.ic_like_posts:
-                    Intent likePost = new Intent(context, LikePostActivity.class);//ACTIVITY_NUM = 3
-                    context.startActivity(likePost
-                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                    break;
-                case R.id.ic_user_profile:
-                    Intent userProfileIntent = new Intent(context, UserProfileActivity.class);//ACTIVITY_NUM = 4
-                    context.startActivity(userProfileIntent
-                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                    break;
+                    case R.id.ic_add_post:
+                        Intent addPostIntent = new Intent(context, AddPostActivity.class);//ACTIVITY_NUM = 2
+                        context.startActivity(addPostIntent
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                        break;
+                    case R.id.ic_like_posts:
+                        Intent likePost = new Intent(context, HistoryLogActivity.class);//ACTIVITY_NUM = 3
+                        context.startActivity(likePost
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                        break;
+                    case R.id.ic_user_profile:
+                        Intent userProfileIntent = new Intent(context, UserProfileActivity.class);//ACTIVITY_NUM = 4
+                        context.startActivity(userProfileIntent
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                        break;
+                }
+                return false;
             }
-            return false;
         });
     }
 }
