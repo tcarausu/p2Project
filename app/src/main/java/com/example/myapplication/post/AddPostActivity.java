583d7f795300297826340b149249a7b7b678b226
tcarausu <tcarausu@gmail.com>
2019-05-21 12:19:59 +0200

    rafi's changes

--- app/src/main/java/com/example/myapplication/post/AddPostActivity.java
+++ app/src/main/java/com/example/myapplication/post/AddPostActivity.java
@@ -1,14 +1,10 @@
 package com.example.myapplication.post;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.myapplication.R;
-import com.example.myapplication.utility_classes.Permissions;
 import com.example.myapplication.utility_classes.SectionsPagerAdapter;
 
 /**
@@ -26,15 +22,7 @@ public class AddPostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
 
-        initLayout();
-        buttonListeners();
-
-        if (checkPermissionsArray(Permissions.PERMISSIONS)) {
-            setupViewPager();
-        } else {
-            verifyPermissions(Permissions.PERMISSIONS);
-        }
-
+        setupViewPager();
 
     }
 
@@ -45,72 +33,6 @@ public class AddPostActivity extends AppCompatActivity {
         mViewPager = findViewById(R.id.container);
         mViewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = findViewById(R.id.tabsBottom);
-        tabLayout.setupWithViewPager(mViewPager);
-//        tabLayout.getTabAt(0).setText(getString(R.string.post));
-
     }
-
-    /**
-     * Used to get tabNumber in fragments
-     *
-     * @return
-     */
-    public int getTabNumber() {
-        return mViewPager.getCurrentItem();
-    }
-
-    /**
-     * Checking all permissions
-     *
-     * @param permissions
-     * @return
-     */
-    public Boolean checkPermissionsArray(String[] permissions) {
-        for (int i = 0; i < permissions.length; i++) {
-            String check = permissions[i];
-            if (!checkPermissions(check)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Checking single permission
-     *
-     * @param permission
-     * @return
-     */
-    public Boolean checkPermissions(String permission) {
-        int permissionRequest = ActivityCompat.checkSelfPermission(AddPostActivity.this, permission);
-
-        if (permissionRequest != PackageManager.PERMISSION_GRANTED) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    public void verifyPermissions(String[] permissions) {
-        ActivityCompat.requestPermissions(
-                AddPostActivity.this,
-                permissions,
-                PERMISSION_REQUEST
-        );
-    }
-
-
-    public void initLayout() {
-
-    }
-
-    public void buttonListeners() {
-
-    }
-
-    /**
-     * Bottom Navigation View setup
-     */
 
 }
