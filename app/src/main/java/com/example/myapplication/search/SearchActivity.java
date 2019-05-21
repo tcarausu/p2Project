4bf238c84c0668db69b2e11ee866e12dc518c6fc
tcarausu <tcarausu@gmail.com>
2019-05-21 18:36:32 +0200

    image+seatup

--- app/src/main/java/com/example/myapplication/search/SearchActivity.java
+++ app/src/main/java/com/example/myapplication/search/SearchActivity.java
@@ -22,7 +22,7 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
+        setContentView(R.layout.search_activity);
 
         initLayout();
         buttonListeners();
