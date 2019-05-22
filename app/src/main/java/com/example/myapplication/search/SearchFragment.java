8c3b7c35f38eefb80e4ad0f9067937373aa4d681
tcarausu <tcarausu@gmail.com>
2019-05-22 15:32:23 +0200

    search activity "working" with exact data

--- app/src/main/java/com/example/myapplication/search/SearchFragment.java
+++ app/src/main/java/com/example/myapplication/search/SearchFragment.java
@@ -9,8 +9,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -21,7 +19,6 @@ import android.widget.Toast;
 import com.example.myapplication.R;
 import com.example.myapplication.home.HomeActivity;
 import com.example.myapplication.models.User;
-import com.example.myapplication.utility_classes.BottomNavigationViewHelper;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -29,7 +26,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,13 +37,11 @@ import static com.facebook.FacebookSdk.getApplicationContext;
  **/
 public class SearchFragment extends Fragment implements View.OnClickListener {
     private static final String TAG = "SearchActivity";
-    private static final int ACTIVITY_NUM = 1;
 
     //widgets
     private EditText mSearchParam;
     private ImageView backArrow;
     private ImageButton mSearchButton;
-    private BottomNavigationViewEx bottomNavigationViewEx;
 
     //Firebase
     private FirebaseAuth mAuth;
@@ -67,7 +61,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
 
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
@@ -76,20 +70,10 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         firebaseDatabase = FirebaseDatabase.getInstance();
         myDatabaseUserRef = FirebaseDatabase.getInstance().getReference("users");
 
-
-        search_recycler_view = view.findViewById(R.id.search_recycler_view_id);
-        search_recycler_view.setHasFixedSize(true);
-        search_recycler_view.setLayoutManager(new LinearLayoutManager(getContext()));
-
         initLayout(view);
         buttonListeners();
-        setupBottomNavigationView();
 
         getUserFromDatabase();
-
-//        GetData getData = new GetData();
-//        getData.execute();
-
 
         return view;
     }
@@ -99,7 +83,9 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         backArrow = view.findViewById(R.id.backArrow);
         mSearchButton = view.findViewById(R.id.search_button_id);
         search_recycler_view = view.findViewById(R.id.search_recycler_view_id);
-        bottomNavigationViewEx = view.findViewById(R.id.bottomNavigationBar);
+        search_recycler_view.setHasFixedSize(true);
+        search_recycler_view.setLayoutManager(new LinearLayoutManager(getContext()));
+
 
     }
 
@@ -111,14 +97,6 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
 
     public void getUserFromDatabase() {
         String keyword = mSearchParam.getText().toString();
-
-//        Query query = myDatabaseUserRef
-//                .orderByChild(myDatabaseUserRef
-//                        .child(getString(R.string.field_user_id))
-//                        .child(getString(R.string.field_username))
-//                        .getKey()
-//                )
-//                ;
 
         myDatabaseUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -136,14 +114,13 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
                         userList.add(user);
                         adapter.setUserList(userList);
                         Log.d(TAG, "onDataChange: profilePic and username :" + mProfilePhoto + " " + mUsername);
-
+                        search_recycler_view.setAdapter(adapter);
 
                     } else {
-                        Toast.makeText(getApplicationContext(), "No such user exists", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(getApplicationContext(), "No such user exists", Toast.LENGTH_SHORT).show();
 
                     }
                 }
-                search_recycler_view.setAdapter(adapter);
 
             }
 
@@ -173,15 +150,4 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
         }
     }
 
-    /**
-     * Bottom Navigation View setup
-     */
-    public void setupBottomNavigationView() {
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(getContext(), bottomNavigationViewEx);
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
-
-    }
 }
